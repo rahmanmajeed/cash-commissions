509fd53ea7d0c5e02689b306982d7e2a77929448
Majedur Rahman <Majedur.Rahman@Brainstation-23.com>
2023-03-16 22:02:40 +0600

    [add] project entry

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('initiate.');
+import fs from 'fs';
+import { CASH_IN, CASH_OUT } from './config/types.js';
+import { calcCashInCommission } from './services/cashIn.js';
+import { calcCashOutCommission } from './services/cashOut.js';
+
+let inputfile = process.argv.slice(2)[0];
+if (!inputfile) {
+    inputfile = 'input.json';
+}
+const inputFilePath = `./${inputfile}`;
+
+const inputs = JSON.parse(fs.readFileSync(inputFilePath));
+
+inputs.forEach((input) => {
+    switch (input.type) {
+        case CASH_IN:
+            console.log(calcCashInCommission(input));
+            break;
+        case CASH_OUT:
+            console.log(calcCashOutCommission(input));
+            break;
+        default:
+            break;
+    }
+});
